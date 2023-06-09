316d33d76d620cc42baa6364cdf57a55175a33ca
HandyyWeb <lebretonandy@gmail.com>
2023-06-09 21:09:11 +0400

    2 waves of adjustements

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider>
-      <Header/>
-      <Main />
+        <Header/>
+        <Main />
     </ThemeProvider>
   </React.StrictMode>
 );
